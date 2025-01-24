444b6989961415aa3b89edd35a3838e047523914
duc-minh-droid <ducminhcsp@gmail.com>
2025-01-24 19:07:06 +0000

    s

--- selab/selab/settings.py
+++ selab/selab/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@6w*&o!_w^&lc3v*4y8n!-)c6#9ho6xbzml4gp!5(tj*cyvz&e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
